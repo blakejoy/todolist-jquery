5e7c03502145cd7e05363b4797a5deef320fbb6e
Blake Joynes <blakejoynes@gmail.com>
2018-10-12 16:39:30 -0400

    initial commit. working on styling

---
+++ assets/js/todos.js
@@ -0,0 +1,22 @@
+
+//second argument says when li clicked in ul run code
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+
+$("ul").on("click", ".delete", function(e){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    })
+    e.stopPropagation(); //Stop event from hitting parent elements
+});
+
+$("input[type='text']").keypress(function(e){
+    if (e.which == 13){
+    var todoText = $(this).val();
+
+    $("ul").append("<li><span class='delete'>X</span> "+ todoText +"</li>");
+    $(this).val(""); 
+    }
+});
